3b62414fe9118320bf53c1db887c0168c6552f8b
anandmathi <anandmathi@gmail.com>
2023-10-14 19:45:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-var request = require('request');
-var express = require('express');
-var app = express();
+const request = require('request');
+const express = require('express');
+const cors = require('cors');
+const app = express();
 
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+app.use(cors({
+      origin: "*"
+    }
+))
+app.options('*', cors())
+
+// app.use(function(req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', '*');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   next();
+// });
 
 app.set('port', process.env.PORT || 3000);
 
